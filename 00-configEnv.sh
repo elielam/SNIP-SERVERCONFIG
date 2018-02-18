d79f0f7936cb3bdb8cfbb183af0d66f27c5fa220
Elie Laloum <34446626+elielam@users.noreply.github.com>
2018-02-18 15:05:06 +0100

    Initial Commit

---
+++ 00-configEnv.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo '######## ENV BASICS ########'
+
+echo 'UPDATE'
+
+echo ''
+
+sudo apt-get update -y
+
+echo ''
+
+echo 'UPGRADE'
+
+echo ''
+
+sudo apt-get upgrade -y
+
+echo ''
+
+echo 'DIST-UPGRADE'
+
+echo ''
+
+read -p 'Would you want to dist-upgrade ( y or n ) : ' choice
+
+echo ''
+
+case "$choice" in
+"y" | "Y")
+    sudo apt-get dist-upgrade -y
+    echo ''
+    echo 'Env is up-to-date'
+    ;;
+"n" | "N")
+    echo 'Env is update and upgrade'
+    ;;
+*)
+    echo 'Confirm with y or n , try again'
+    ;;
+esac
---
+++ 01-configNetwork.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+echo '######## CONFIG NETWORK ########'
+echo ''
+echo 'CONFIG STATIC IP'
+
+if [ $1 = "ipv4" ]; then
+     ipv4Adress=$2
+elif [ $1 = "ipv6" ]; then
+	echo 'ipv6'
+     ipv6Adress=$2
+elif [ $1 = "both" ]; then
+     ipv4Adress=$2
+     ipv6Adress=$3
+fi
+
+echo ''
+
+read -p 'Enter interface name : ' interface
+
+echo ''
+
+read -p 'Enter gateway adress : ' gateway
+
+echo ''
+
+	{
+    		echo '# This file describes the network interfaces available on your system'
+		echo '# For more information, see netplan(5).'
+		echo 'network:'
+		echo ' version: 2'
+		echo ' renderer: networkd'
+		echo ' ethernets:'
+		echo "   $interface:"
+		echo '     dhcp4: no'
+		echo '     dhcp6: no'
+
+		if [ $1 = "ipv4" ]; then
+    			echo "     addresses: [$ipv4Adress/24]"
+		elif [ $1 = "ipv6" ]; then
+	     		echo "     addresses: [$ipv6Adress/64]"
+		elif [ $1 = "both" ]; then
+	    		echo "     addresses: [$ipv4Adress/24, '$ipv6Adress/64']"
+		fi
+
+		echo "     gateway4: $gateway"
+		echo '     nameservers:'
+		echo '       addresses: [8.8.8.8,8.8.4.4]'
+	} >/etc/netplan/01-netcfg.yaml
+
+echo 'File 01-netcfg.yaml was created in /etc/netplan/'
+
+echo ''
+
+echo 'NETPLAN DEBUG'
+echo ''
+sudo netplan --debug generate
+
+echo ''
+
+echo 'NETPLAN APPLY'
+sudo netplan apply
+echo ''
+
+echo 'CONFIG FINISHED'
+echo ''
+
+# Modify in future to test if its ipv4 or ipv6 or both and display appropriate message
+echo "Interface $interface is know configured on $ipv4Adress"
+echo "The gateway is $gateway"
+
+echo ''
